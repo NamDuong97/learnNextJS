d8587e4a11a6e708f5e78d4da73c64c2a41a1871
Nam Tran <nakani3797@gmail.com>
2025-12-03 10:56:53 +0700

    Tối ưu hoá hình ảnh bằng component Image có sẵn của next

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* component Image để tối ưu ảnh thay vì sử dụng img */}
           <Image
             src="/hero-desktop.png"
             width={1000}
